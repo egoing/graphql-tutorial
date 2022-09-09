0e0d322e8af639fc9d3c06c5b009a67f896f4ded
egoing <egoing@gmail.com>
2022-09-09 16:16:40 +0900

    schema language, query language

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const express = require('express'); // npm install express
 const app = express();
 const port = 3010;
 const path = require('path');
+const { buildSchema } = require('graphql'); // npm install graphql
+var { graphqlHTTP } = require('express-graphql'); // npm install express-graphql
+
+const schema = buildSchema(`
+    type Query {
+        title:String
+    }
+`)
+
+var root = {
+    title:'egoing blog',
+}
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root
+}));
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('index.html'));
 });
