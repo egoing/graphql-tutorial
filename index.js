b0c80454d53d9efefc72e1070162b329d6f5d284
egoing <egoing@gmail.com>
2022-09-09 16:28:44 +0900

    복수의 필드 사용

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const { buildSchema } = require('graphql'); // npm install graphql
 var { graphqlHTTP } = require('express-graphql'); // npm install express-graphql
 
 const schema = buildSchema(`
+    type Topic{
+        id:Int
+        title:String
+        body:String
+    }
     type Query {
         title:String
+        topics:[Topic]
     }
 `)
 
+const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'}
+]
+
 var root = {
     title:'egoing blog',
+    topics:topics
 }
 
 app.use('/graphql', graphqlHTTP({
